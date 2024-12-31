3f387397f0bb71d1d7d21f2eecb3fb1f789392c4
Yaman Bhandari <bhandariyaman198@gmail.com>
2024-12-31 14:36:11 +0545

    Move addToCart into cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-  import {cart} from '../data/cart.js';
+  import {cart,addToCart} from '../data/cart.js';
   import {products} from '../data/products.js';
   let productsHTML=''; 
 
@@ -58,23 +58,13 @@
   document.querySelector('.js-products-grid').
   innerHTML = productsHTML;
 
+  function updateCartQuantity(){
+    let cartQuantity = 0;
+    cart.forEach((cartItem) => {
+      cartQuantity += cartItem.quantity;
+    });
 
-  function addToCart(productId){
-        let matchingItem;
-        cart.forEach((item) => {
-          if(productId === item.productId){
-             matchingItem = item;
-          }
-      });
-  
-      if(matchingItem){
-        matchingItem.quantity += 1;
-      }else{
-        cart.push({
-          productId: productId,
-          quantity : 1
-        }); 
-      }
+    document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
   }
 
   document.querySelectorAll('.js-add-to-cart').
@@ -82,13 +72,7 @@
     button.addEventListener('click', () => {
       const productId = button.dataset.productId;
       addToCart(productId);
-  
-    let cartQuantity = 0;
-    cart.forEach((item) => {
-      cartQuantity += item.quantity;
-    });
-
-    document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
+      updateCartQuantity();
     });
   });
 
