54004bf98198a0e78f785ff5fe849002a9334363
Yaman Bhandari <bhandariyaman198@gmail.com>
2025-01-02 13:59:19 +0545

    Save data and generate HTML for delivery options

---
+++ data/deliveryOptions.js
@@ -0,0 +1,13 @@
+export const deliveryOptions =[{
+   id: '1',
+   devileryDays:7,
+   priceCents:0
+  },{
+    id:'2',
+    devileryDays: 3,
+    priceCents: 499
+  },{
+    id:'3',
+    devileryDays: 1,
+    priceCents:999
+  }];
