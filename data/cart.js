7b1f659c925e6ee334024e4e8d332a0cb3dce0a3
Yaman Bhandari <bhandariyaman198@gmail.com>
2025-01-02 14:09:49 +0545

    Make delivery options interactive

--- data/cart.js
+++ data/cart.js
@@ -9,7 +9,6 @@ if (!cart) {
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
     quantity: 1,
     deliveryOptionId: '2'
-
   }];
 }
 
@@ -49,6 +48,20 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart;
+
+  saveToStorage();
+}
+
+export function updateDeliveryOption(productId, deliveryOptionId){
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+
+  matchingItem.deliveryOptionId = deliveryOptionId;
 
   saveToStorage();
 }
