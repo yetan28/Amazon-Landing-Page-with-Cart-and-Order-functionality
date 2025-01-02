1312456a14c7d897b75ce1880b5bef0946c2b4c0
Yaman Bhandari <bhandariyaman198@gmail.com>
2025-01-02 12:01:29 +0545

    delete products from the cart when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
- export const cart=[{
+ export let cart=[{
   productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2
  },{
@@ -23,4 +23,17 @@ if(matchingItem){
     quantity : 1
   }); 
 }
+}
+
+function removeFromCart(productId){
+  const newCart =[];
+
+  cart.forEach((cartItem) => {
+     if(cartItem !== productId){
+      newCart.push(cartItem);
+     }
+  });
+
+  cart = newCart;
+
 }
