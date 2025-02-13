9a5d882be8b222482c407c1c83a53864128bddce
Yaman Bhandari <bhandariyaman198@gmail.com>
2025-02-13 09:39:36 +0545

    Load products from the backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,11 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
-import '../data/backend-practice.js';
-//import '../data/cart-class.js';
+import { loadProducts } from '../data/products.js';
 
-renderOrderSummary();
-renderPaymentSummary();
+//import '../data/cart-class.js';
+//import '../data/backend-practice.js';
+
+loadProducts(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+});
